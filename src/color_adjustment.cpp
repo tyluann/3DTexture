2877b0e082bf1131462f71c6986bbbc0f42a00ca
tyluann <13264062239@163.com>
2018-08-18 16:06:00 +0800

    新建工程，添加opencv和eigen

---
+++ src/color_adjustment.cpp
@@ -0,0 +1,3 @@
+
+#include <opencv2/opencv.hpp>
+#include <Eigen/Eigen>
---
+++ src/demo.cpp
@@ -0,0 +1,8 @@
+
+#include<iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "hello texture !" << std::endl;
+	return 0;
+}
